f56f447f83a4ab584e8ac9b09aa07ba80409b9d2
ntan1055 <43207464+ntan1055@users.noreply.github.com>
2019-02-04 08:25:01 -0500

    Update sortAManiaTestRunner.java

--- src/sortAManiaTestRunner.java
+++ src/sortAManiaTestRunner.java
@@ -5,17 +5,33 @@ public class sortAManiaTestRunner
     public static void main(String[] args)
     {
         SortCompetition team2 = new Team2SortCompetition();
-        int[] randIntArr = RandIntArr.randIntArr(10000);
+        int[] randIntArr = randIntArr(10000);
 
         System.out.println("Unsorted");
         printArr(randIntArr);
 
         long time = System.currentTimeMillis();
         int median = Team2SortCompetition.challengeOne(randIntArr);
+        time = System.currentTimeMillis() - time;
+        System.out.println("Challenge One Time Taken: " + time * 0.001 + " Seconds");
+        System.out.println("Median equals: " + median);
+
+        System.out.println("Sorted");
+        printArr(randIntArr);
     }
 
     private static void printArr(int[] randIntArr)
     {
         System.out.println(Arrays.toString(randIntArr));
+    }
+
+    private static int[] randIntArr(int count)
+    {
+        int[] arr = new int[count];
+        for(int i = 0; i < count; i++)
+        {
+            arr[i] = (int)(Math.random() * 10001);
+        }
+        return arr;
     }
 }
