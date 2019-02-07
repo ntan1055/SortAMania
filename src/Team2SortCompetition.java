a1696db067c0ab75f92d90a86d175d4e310e8f73
yhuang7593 <yhuang7593@bths.edu>
2019-02-06 21:52:02 -0500

    :))

--- src/Team2SortCompetition.java
+++ src/Team2SortCompetition.java
@@ -1,8 +1,6 @@
-public class Team2SortCompetition extends SortCompetition
-{
+public class Team2SortCompetition extends SortCompetition {
     @Override
-    public int challengeOne(int[] arr)
-    {
+    public int challengeOne(int[] arr) {
         return 0;
     }
 
@@ -10,13 +8,11 @@ public class Team2SortCompetition extends SortCompetition
     public int challengeTwo(String[] arr, String query) {
         int num = 10000;
         arr = new String[num];
-        while (num > 0)
-        {
+        while (num > 0) {
             int i = 0;
             String s = "";
-            while (i < 5)
-            {
-                char c = (char)((Math.random()*26)+97);
+            while (i < 5) {
+                char c = (char) ((Math.random() * 26) + 97);
                 s = s + c;
                 i++;
             }
@@ -31,18 +27,28 @@ public class Team2SortCompetition extends SortCompetition
         return 0;
     }
 
-     @Override
-    public int challengeFour(int[][] arr)
-    {
-        int[] medianArr = new int[arr.length];
-        for(int row = 0; row < arr.length; row++)
-        {
-            for(int col = 0; col < arr.length; col++)
-            {
-                //sort sub arrays
+    int partition(int arr[], int low, int high) {
+        int pivot = arr[high];
+        int i = (low - 1);
+        int temp;
+        for (int j = low; j <= high - 1; j++) {
+            if (arr[j] <= pivot) {
+                i++;
+                temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
             }
-            //median finder here
-            //add median to the medianArr
+        }
+        temp = arr[i + 1];
+        arr[i + 1] = arr[high];
+        arr[high] = temp;
+        return (i + 1);
+    }
+    void quickSort(int arr[], int low, int high) {
+        if (low < high) {
+            int pi = partition(arr, low, high);
+            quickSort(arr, low, pi - 1);
+            quickSort(arr, pi + 1, high);
         }
     }
 
