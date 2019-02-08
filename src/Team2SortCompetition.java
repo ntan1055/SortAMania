d51e79d1a23efdf2018201346be404bac36dac45
yhuang7593 <yhuang7593@bths.edu>
2019-02-08 08:39:34 -0500

    ;)

--- src/Team2SortCompetition.java
+++ src/Team2SortCompetition.java
@@ -1,3 +1,4 @@
+java.util.Arrays;
 public class Team2SortCompetition extends SortCompetition {
 
 
@@ -66,32 +67,36 @@ public class Team2SortCompetition extends SortCompetition {
     @Override
     public int challengeThree(int[] arr) {
         return 0;
-    }
-
-    int partition(int arr[], int low, int high) {
-        int pivot = arr[high];
-        int i = (low - 1);
-        int temp;
-        for (int j = low; j <= high - 1; j++) {
-            if (arr[j] <= pivot) {
-                i++;
-                temp = arr[i];
-                arr[i] = arr[j];
-                arr[j] = temp;
+        class BubbleSort {
+            public void bubbleSort(int[] Arr) {
+                int temp;
+                int j;
+                for (j = 0; j < Arr.length - 1; j++) {
+                    for (int i = 0; i < Arr.length - 1; i++) {
+                        if (Arr[i] > Arr[i + 1]) {
+                            temp = Arr[i + 1];
+                            Arr[i + 1] = Arr[i];
+                            Arr[i] = temp;
+                        }
+                    }
+                }
             }
         }
-        temp = arr[i + 1];
-        arr[i + 1] = arr[high];
-        arr[high] = temp;
-        return (i + 1);
-    }
-    void quickSort(int arr[], int low, int high) {
-        if (low < high) {
-            int pi = partition(arr, low, high);
-            quickSort(arr, low, pi - 1);
-            quickSort(arr, pi + 1, high);
+
+        public static int median(int[],arr[])
+        {
+            int middle = arr.length / 2;
+            int median=0;
+            if (arr.length%2==1)
+                return median=arr[middle];
+            else
+                return median=(arr[middle-1]+arr[middle])/2;
+            return median;
         }
     }
+
+
+
     
      @Override
     public int challengeFour(int[][] arr)
